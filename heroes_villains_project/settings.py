7581db3c043bcfe80c5af3fae1033f3fd7aa92bf
psions <psionreality@gmail.com>
2022-03-17 11:57:25 -0500

    changed database settings + created Heroes app + added super_types + super file

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4sdh)@dlt7agmd+q)a)zygd(18k25yz(g--ir2w8^taj0o*@1m'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -113,3 +112,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from heroes_villains_project.local_settings import *
+except ImportError:
+    pass
