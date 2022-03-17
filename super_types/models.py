ca4fe12573f7f754307c8121f256cd7b5b95a889
psions <psionreality@gmail.com>
2022-03-17 15:07:52 -0500

    Changed database settings + created heroes app

--- super_types/models.py
+++ super_types/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.forms import CharField
+
 
 # Create your models here.
+
+class super_type(models.Model):
+    type = CharField(max_length=255)
--- supers/models.py
+++ supers/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 from django.forms import CharField
+from django.contrib.auth.models import User
 
-type - CharField
+
+
+
+
 
 # Create your models here.
+
+
+class super(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    second_ability = models.CharField(max_length=255)
+    catchphrase = models.CharField(max_length=255)
+    super_type = models.ForeignKey(User, on_delete=models.CASCADE)
