02a40e42ccb2b9a6bebc5dbbd3bc741e54794ae7
psions <psionreality@gmail.com>
2022-03-17 16:28:35 -0500

    updated admin + views file for supers

--- supers/admin.py
+++ supers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import super
 
 # Register your models here.
+admin.site.register(super)
--- supers/views.py
+++ supers/views.py
@@ -1,8 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer import SuperSerializer
+from .models import super
+
 
 
 @api_view(['GET'])
 def supers_list(request):
+    supers = super.objects.all()
 
-    return Response('ok')
+    serializer = SuperSerializer(supers, many=True)
+
+    
+    return Response(serializer.data)
