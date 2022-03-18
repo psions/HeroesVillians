e63624e081763e541782f8073c729c1f5abfddc1
psions <psionreality@gmail.com>
2022-03-18 17:01:03 -0500

    added integerfield for FK to be used on put

--- supers/serializer.py
+++ supers/serializer.py
@@ -7,3 +7,5 @@ class SuperSerializer(serializers.ModelSerializer):
         fields = ['id', 'name', 'alter_ego', 'primary_ability', 'second_ability', 'catchphrase', 'super_type_id']
         depth = 1
     
+    super_type_id = serializers.IntegerField(write_only=True)
+    
